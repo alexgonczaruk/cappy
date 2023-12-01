cca137d40d9fb8a68c05c37324ea2d0abcdc4ee5
shehanbandara <s2bandar@uwaterloo.ca>
2023-11-30 22:27:37 -0500

    Fixed Compass

--- test_compass.py
+++ test_compass.py
@@ -1,16 +1,34 @@
-import gy271compass as GY271
+# import gy271compass as GY271
+# from time import sleep
+
+# sensor = GY271.compass(address=0x0d)
+
+# print('[Press CTRL + C to end the script!]')
+# try:
+#     while True:
+#         angle = sensor.get_bearing()       
+#         temp = sensor.read_temp()
+    
+#         print('Heading Angle = {}°'.format(angle))
+#         print('Temperature = {:.1f}°C'.format(temp))
+#         sleep(2)
+
+# except KeyboardInterrupt:
+#     print('\nScript end!')
+
+# git clone https://github.com/RigacciOrg/py-qmc5883l.git to use import
+import py_qmc5883l
 from time import sleep
 
-sensor = GY271.compass(address=0x0d)
+sensor = py_qmc5883l.QMC5883L()
+sensor.declination = 9.46
+sensor.calibration = [[1.6330085087384014, 0.0323886463392132, 1763.0899153395242], [0.032388646339213206, 1.0016572042827312, 2448.0388464736657], [0.0, 0.0, 1.0]]
 
 print('[Press CTRL + C to end the script!]')
 try:
     while True:
         angle = sensor.get_bearing()       
-        temp = sensor.read_temp()
-    
         print('Heading Angle = {}°'.format(angle))
-        print('Temperature = {:.1f}°C'.format(temp))
         sleep(2)
 
 except KeyboardInterrupt:
