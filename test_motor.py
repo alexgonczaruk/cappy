dc4a8d13ca170dd0463561df10da4f24c74928b1
alexgonczaruk <55724886+alexgonczaruk@users.noreply.github.com>
2024-01-17 00:17:53 -0500

    other wheel

--- test_motor.py
+++ test_motor.py
@@ -5,9 +5,13 @@ filedescriptors = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin)
 keyInput = 0
 
-# 32 = the motor PWM connection
-# 31 = be stop
-# 29 = the reverse connection
+# 32 = the motor PWM connection, GPIO 12
+# 31 = be stop, GPIO 6
+# 29 = the reverse connection, GPIO 5
+
+# 33 - motor PWM connection, GPIO 13
+# 36 - be stop, GPIO 16
+# 37 - reverse connection, GPIO 26
 
 def init():
     GPIO.setmode(GPIO.BOARD)
@@ -15,6 +19,10 @@ def init():
     GPIO.setup(29, GPIO.OUT)
     GPIO.setup(31, GPIO.OUT)
     GPIO.setup(32, GPIO.OUT)
+    
+    GPIO.setup(33, GPIO.OUT)
+    GPIO.setup(36, GPIO.OUT)
+    GPIO.setup(37, GPIO.OUT)
 
 def forward(tf): 
     GPIO.output(29, False)
@@ -22,6 +30,13 @@ def forward(tf):
     motorl = GPIO.PWM(32, 50)
     motorl.start(0)
     motorl.ChangeDutyCycle(20)
+
+    GPIO.output(36, False)
+    GPIO.output(37, False)
+    motorl = GPIO.PWM(33, 50)
+    motorl.start(0)
+    motorl.ChangeDutyCycle(20)
+
     time.sleep(tf)
 
 def reverse(tf): 
@@ -30,10 +45,18 @@ def reverse(tf):
     motorl = GPIO.PWM(32, 50)
     motorl.start(0)
     motorl.ChangeDutyCycle(20)
+
+    GPIO.output(37, True)
+    GPIO.output(36, False)
+    motorl = GPIO.PWM(33, 50)
+    motorl.start(0)
+    motorl.ChangeDutyCycle(20)
+
     time.sleep(tf)
 
 def brake(tf): 
     GPIO.output(31, True)
+    GPIO.output(36, True)
     time.sleep(tf)
 
 # Init GPIO
