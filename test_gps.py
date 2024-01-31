ff877bd242f5db28d487e02df21ba3001ba58b07
shehanbandara <s2bandar@uwaterloo.ca>
2024-01-31 02:52:19 +0000

    Combining socket and gps

--- test_gps.py
+++ test_gps.py
@@ -1,20 +1,39 @@
 import time
 import serial
-
 import adafruit_gps
 
-
 uart = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
-
 gps = adafruit_gps.GPS(uart, debug=False)
-
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-
 gps.send_command(b'PMTK220,1000')
 
-last_print = time.monotonic()
-while True:
+lat_avg = []
+lon_avg = []
+MAX_LIST_SIZE = 20
 
+last_print = time.monotonic()
+
+def get_rpi_coordinates(last = time.monotonic(), lat_avg = [], lon_avg = []):
+    gps.update()
+
+    current = time.monotonic()
+    if current - last >= 1.0:
+        if not gps.has_fix:
+            return 0, 0, last, lat_avg, lon_avg
+
+        lat_avg.append(gps.latitude)
+        lon_avg.append(gps.longitude)
+
+        if len(lat_avg) > MAX_LIST_SIZE:
+            lat_avg.pop(0)
+        if len(lon_avg) > MAX_LIST_SIZE:
+            lon_avg.pop(0)
+
+        print('Latitude: {0:.6f} degrees'.format(sum(lat_avg)/len(lat_avg)))
+        print('Longitude: {0:.6f} degrees'.format(sum(lon_avg)/len(lon_avg)))
+        return sum(lat_avg)/len(lat_avg), sum(lon_avg)/len(lon_avg), last, lat_avg, lon_avg
+
+while True:
     gps.update()
 
     current = time.monotonic()
@@ -23,6 +42,18 @@ while True:
         if not gps.has_fix:
             print('Waiting for fix...')
             continue
+
+        lat_avg.append(gps.latitude)
+        lon_avg.append(gps.longitude)
+
+        if len(lat_avg) > CAP:
+            lat_avg.pop(0)
+        if len(lon_avg) > CAP:
+            lon_avg.pop(0)
+
         print('=' * 40)  # Print a separator line.
-        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+        # print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        # print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+
+        print('Latitude: {0:.6f} degrees'.format(sum(lat_avg)/len(lat_avg)))
+        print('Longitude: {0:.6f} degrees'.format(sum(lon_avg)/len(lon_avg)))
--- test_socket.py
+++ test_socket.py
@@ -1,5 +1,6 @@
 import socket
 import random
+from test_gps import get_rpi_coordinates
 from test_obstacle_detection import detection
 from math import cos, asin, sqrt, pi
 
@@ -17,6 +18,10 @@ print("connected to client!")
 ACTION = "ACTION"
 COORDINATE = "COORDINATE"
 
+lat_avg = []
+lon_avg = []
+last = time.monotonic()
+
 def distance(lat1, lon1, lat2, lon2):
     r = 6371000 # km
     p = pi / 180
@@ -26,26 +31,29 @@ def distance(lat1, lon1, lat2, lon2):
 
 while True:
     message = connectionSocket.recv(1024).decode()
-    
     code, content = message.split(":", 1)
+
     if code == ACTION:
         print(f"Incoming action: {content}")
     else:
-        current_location_lat = 43.476427
-        current_location_lon = -80.538543
+        if lat_avg == []:
+            pi_location_lat, pi_location_lon, last, lat_avg, lon_avg = get_rpi_coordinates()
+        else:
+            pi_location_lat, pi_location_lon, last, lat_avg, lon_avg = get_rpi_coordinates(last, lat_avg, lon_avg)
 
+        # Parse the phone's coordinates
         phone_location = content.split(", ")
         phone_location_lat = float(phone_location[0])
         phone_location_lon = float(phone_location[1])
         print(f"Coordinates: {phone_location_lat}, {phone_location_lon}")
 
-        distance_from_phone = distance(current_location_lat, current_location_lon, phone_location_lat, phone_location_lon)
-
+        distance_from_phone = distance(pi_location_lat, pi_location_lon, phone_location_lat, phone_location_lon)
         distance_from_phone = "PI:" + str(round(distance_from_phone, 2))
 
         # FOR NOW KEEPING THE BELOW CODE HERE BECAUSE WE HAVE THE SOCKET HERE AS WELL
         if detection():
             connectionSocket.send("ACTION:STOP".encode())
 
-        print(distance_from_phone)
+        print(f"Pi's distance from phone: {distance_from_phone}")
         connectionSocket.send(distance_from_phone.encode())
+        
